89cd1e49be32e7b35f1de074c11bf6cfac75372d
edaa-adgzl <adiguzeleda87@gmail.com>
2021-11-06 00:48:16 +0300

    first

---
+++ src/myutilss/Main.java
@@ -0,0 +1,21 @@
+
+package myutilss;
+
+import static myutilss.MyUtilss.cokgenAlan;
+import static myutilss.MyUtilss.cokgenCevre;
+import static myutilss.MyUtilss.fx1;
+import static myutilss.MyUtilss.fx2;
+import static myutilss.MyUtilss.ucgenAlan;
+import static myutilss.MyUtilss.ucgenCevre;
+
+
+public class Main {
+     public static void main(String[] args) {
+        ucgenCevre();
+        ucgenAlan();
+        cokgenCevre();
+        cokgenAlan();
+        fx1();
+        fx2();
+    }
+}
